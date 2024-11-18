58da3842fe66cb7234f2a511ff3c147a6a5db022
bigge <pongpich.sing@gmail.com>
2024-11-19 00:55:42 +0700

    update cloudflared deployment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # devops-setup
+
+## Getting starts
+
+We will use Cloudflared Tunnel on our kubernetes setup (microk8s in my case). Please follow [this](https://developers.cloudflare.com/cloudflare-one/tutorials/many-cfd-one-tunnel/).
--- scripts/setup-microk8s.sh
+++ scripts/setup-microk8s.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-# Install Microk8s
-sudo snap install microk8s --classic
-
-# Add user to Microk8s group
 sudo usermod -aG microk8s $USER
 sudo chown -R $USER ~/.kube
 newgrp microk8s
 
 # Enable add-ons
-microk8s enable dns ingress storage
+microk8s enable dns
+microk8s enable ingress
+microk8s enable storage
 
-# Install Docker
-sudo apt update
-sudo apt install -y docker.io
 sudo usermod -aG docker $USER
 newgrp docker
 
-# Create Cloudflared secret
-microk8s.kubectl create secret generic cloudflared-token \
-  --from-literal=token='<your-cloudflare-tunnel-token>'
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+sudo dpkg --install cloudflared-linux-amd64.deb
+
+exec "$SHELL"
+
+# Cloudflared Setup
+cloudflared tunnel login
+
+# cloudflared tunnel create <YOUR TUNNEL NAME>
+# kubectl create secret generic tunnel-credentials --from-file=credentials.json=<YOUR FILE>
 
 echo "Microk8s setup completed successfully!"
